a363f862e56c196f9d82259cc69e918d1b037551
goni <gonifs92@gmail.com>
2022-02-05 13:10:08 +0100

    logic error, after getting highest divisible number it should always decrement the power by 1

--- assembly_language_step_by_step_programming_with_linux/chapter2/base10_to_base16_digit_converter.py
+++ assembly_language_step_by_step_programming_with_linux/chapter2/base10_to_base16_digit_converter.py
@@ -4,7 +4,7 @@ A "digit" being a number in base10 that should be interpreted in base16
 '''
 
 #########################
-base10Number = 74
+base10Number = 264
 convertToBaseNumber = 16
 #########################
 
@@ -29,13 +29,19 @@ def get_highest_divisible_number(number, baseNumber):
         divisibleNumber = baseNumber ** i
     return baseNumber ** (i-1)
 
-highest_divisible_number = get_highest_divisible_number(base10Number, convertToBaseNumber)
 result = []
 
-while highest_divisible_number != 1:
-    highest_divisible_number = get_highest_divisible_number(base10Number, convertToBaseNumber)
-    convertedNumberInBase10 = int(base10Number / highest_divisible_number)
+divisible_number = get_highest_divisible_number(base10Number, convertToBaseNumber)
+
+convertedNumberInBase10 = int(base10Number / divisible_number)
+result.append(convertedNumberInBase10)
+base10Number = base10Number - (divisible_number * convertedNumberInBase10)
+
+
+while divisible_number != 1:
+    divisible_number = divisible_number / convertToBaseNumber
+    convertedNumberInBase10 = int(base10Number / divisible_number)
     result.append(convertedNumberInBase10)
-    base10Number = base10Number - (highest_divisible_number * convertedNumberInBase10)
+    base10Number = base10Number - (divisible_number * convertedNumberInBase10)
 
 print(result)
